fb6734fab9c4be628137c82c2e67e7cf17e7f3b1
Software9-1 <dds2508@gmail.com>
2024-09-13 01:12:31 +0300

    v1

---
+++ tests/test.cpp
@@ -0,0 +1,56 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "header.h"
+#include <string>
+
+
+
+TEST(Fan, SetCircuitTringle) {
+    Fan fan;
+    
+    testing::internal::CaptureStdout();
+    fan.SetCircuitTringle();
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "SetPower In Tringle toplogy circuit\n");
+    
+    testing::internal::CaptureStdout();
+    fan.SetCircuitStar();
+    output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "SetPower In star toplogy circuit\n");
+    
+    testing::internal::CaptureStdout();
+    fan.ConnectPower();
+    output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "Connect Power\n");
+    
+    testing::internal::CaptureStdout();
+    fan.DisconnectPower();
+    output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "Disconnect Power\n");
+    
+    testing::internal::CaptureStdout();
+    fan.Delay(2000);
+    output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "wait for 2000 miliseconds\n");
+}
+
+TEST(Controller, TurnOnFanCommand) {
+    Controller controller;
+    Fan fan;
+    controller.SetCommand(new TurnOnFanCommand(&fan));
+    testing::internal::CaptureStdout();
+    controller.ButtonClicked();
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "SetPower In star toplogy circuit\nConnect Power\nwait for 20000 miliseconds\nSetPower In Tringle toplogy circuit\n");
+}
+
+TEST(Controller, TurnOffFanCommand) {
+    Controller controller;
+    Fan fan;
+    controller.SetCommand(new TurnOnFanCommand(&fan));
+    testing::internal::CaptureStdout();
+    controller.ButtonClicked();
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "SetPower In star toplogy circuit\nConnect Power\nwait for 20000 miliseconds\nSetPower In Tringle toplogy circuit\n");
+}
+
